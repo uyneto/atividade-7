c26cd536345badd33d02c3cfdef82be2c9ffbc71
uyneto <90873610+uyneto@users.noreply.github.com>
2021-11-09 19:28:07 -0300

    Add files via upload

---
+++ loginProjetoPadaria.php
@@ -0,0 +1,69 @@
+
+<?php
+
+use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Artisan;
+
+/*
+|--------------------------------------------------------------------------
+| Console Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of your Closure based console
+| commands. Each Closure is bound to a command instance allowing a
+| simple approach to interacting with each command's IO methods.
+|
+*/
+
+Artisan::command('inspire', function () {
+    $this->comment(Inspiring::quote());
+})->purpose('Display an inspiring quote');
+
+
+<h1> ROTAS DO PROJETO PADARIA</h1>
+
+<header>
+<meta charset="utf - 8">
+<meta http-equiv="x-ua-compatible"content=ie=edge">
+<meta nome="uiewport" content=wiath=divece-"width,inicial - scale=1,0"
+<title> ATIVIDADE 6 LTP3  </title>
+<style>
+ body {
+  font-family: arial, helvetica, sans - serif
+  background - imagens: gradint( to rigth, rgb(20,147,220), rgb(17,54,71);
+.box {
+position: absolute;
+top: 50%
+left: 50%
+transforn: (-50% - 50%)
+background - color: rgba("0,0,0,0,8")
+padding:15px;
+border - radius; 15px;
+width: 20%
+
+</style>
+
+</head>
+<body>
+<div class = "box">
+    <form action= "">
+      <fieldset>
+      
+      <h1> <center> Formulario De Login </center> <h1</>
+
+      <ul>
+
+
+<form>
+   <label for= "input nome" > Digite O Login </b> </label>
+   <input type; "text" id="inputNome"> <br> <br> <br> 
+   <label for= "input nome"; > Digite A Senha </label>
+   <input type; "text" id="inputNome"> <br> <br> <br>
+   <button>Acessar</button>
+   
+      </fieldset> 
+     </form>
+    </div>
+   </body>
+  </html>  
+
